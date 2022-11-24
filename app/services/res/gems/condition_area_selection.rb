9402703bfd196dca211273209527ca640cdeaf30
Sreeram <sreeramramasubramanian@NH-LT-0063.local>
2022-11-24 12:47:36 +0530

    Handled condition where they can opt into both Diabetes and Hypertension

--- app/services/res/gems/condition_area_selection.rb
+++ app/services/res/gems/condition_area_selection.rb
@@ -5,7 +5,7 @@ module Res
   module Gems
     class ConditionAreaSelection < Res::Gems::Base
 
-      attr_accessor :exotel_params, :parsed_exotel_params, :res_user, :condition_area_id
+      attr_accessor :exotel_params, :parsed_exotel_params, :res_user, :condition_area_ids
 
       def initialize(logger, exotel_params)
         super(logger)
@@ -19,7 +19,14 @@ module Res
         self.parsed_exotel_params = ExotelWebhook::ParseExotelParams.(self.exotel_params)
 
         # extract condition area of the user from params
-        self.condition_area_id = ConditionArea.id_for(self.exotel_params[:condition_area].to_s)
+        case self.exotel_params[:condition_area]
+        when "diabetes"
+          self.condition_area_ids = [ConditionArea.id_for(self.exotel_params[:condition_area].to_s)]
+        when "hypertension"
+          self.condition_area_ids = [ConditionArea.id_for(self.exotel_params[:condition_area].to_s)]
+        when "d_and_h"
+          self.condition_area_ids = [ConditionArea.ids_for(:diabetes, :hypertension)]
+        end
         self.logger.info("Condition area chosen by user: #{ConditionArea.find(self.condition_area_id).name}")
 
         # check if the user already exists
@@ -27,25 +34,28 @@ module Res
         if self.res_user.blank?
           self.errors = "User not found in DB"
         else
-          unless self.res_user.update(condition_area_id: self.condition_area_id)
-            self.errors = self.res_user.errors.full_messages
-          end
+          # unless self.res_user.update(condition_area_id: self.condition_area_id)
+          #   self.errors = self.res_user.errors.full_messages
+          # end
 
           # add user to the respective condition area
-          self.res_user.add_condition_area(NooraProgram.id_for(:gems), self.condition_area_id)
+          self.condition_area_ids.each do |ca_id|
+            self.res_user.add_condition_area(NooraProgram.id_for(:gems), ca_id)
+          end
 
           # To update the signup tracker, we will look for the existing active tracker.
           # For that object, if condition_area_id is nil they are signing up for GEMS again (because we would have created the tracker on the language selection operation),
           # so we can update the attribute to the right condition area
           # If condition_area_id already exists, make that inactive and create a new user signup tracker
-          active_tracker = self.res_user.active_signups.where(noora_program_id: NooraProgram.id_for(:gems)).first
-          if active_tracker.condition_area_id.present?
-            active_tracker.update(active: false)
-            new_tracker = active_tracker.dup
-            new_tracker.update(condition_area_id: self.condition_area_id, active: true)
-          else
-            active_tracker.update(condition_area_id: self.condition_area_id)
-          end
+
+          # active_tracker = self.res_user.active_signups.where(noora_program_id: NooraProgram.id_for(:gems)).first
+          # if active_tracker.condition_area_id.present?
+          #   active_tracker.update(active: false)
+          #   new_tracker = active_tracker.dup
+          #   new_tracker.update(condition_area_id: self.condition_area_id, active: true)
+          # else
+          #   active_tracker.update(condition_area_id: self.condition_area_id)
+          # end
 
         end
 
