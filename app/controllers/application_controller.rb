bafdddd2d39edf8410d41b2d5479ee12cfd3b15c
abhishek-compro <abhishek.prasad@noorahealth.org>
2023-02-08 18:06:23 +0530

    Handle errors in app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  include Errors
+
+  around_action :handle_errors
+
+  def handle_errors
+    begin
+      yield
+    rescue ErrorResponse => error
+      render status: error.status, json: error.data
+    end
+  end
+
 end
