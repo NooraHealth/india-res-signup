5020abcf28996f00b290109aa98b7d6236dc2874
abhishek-compro <abhishek.prasad@noorahealth.org>
2023-02-08 18:06:23 +0530

    Handle errors in app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  include Errors
+
+  around_action :handle_errors
+
+  def elog error
+    dirname = "#{Rails.root}/log/errors/#{controller_path}"
+    FileUtils.mkdir_p dirname
+
+    logger = Logger.new("#{dirname}/#{action_name}.log")
+    logger.warn(error)
+  end
+
+  def handle_errors
+    begin
+      yield
+    rescue ::ActionController::ParameterMissing => error
+      message = "Missing parameter: '#{error.param}'"
+      elog message
+      render status: 400, json: {error: message, code: "PARAM_MISSING"}
+    rescue ErrorResponse => error
+      elog error.message
+      render status: error.status, json: error.data
+    end
+  end
+
 end
