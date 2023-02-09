2171e0254e6f2f696ea34fdbf607fa064fe4bee6
abhishek-compro <abhishek.prasad@noorahealth.org>
2023-02-09 11:45:36 +0530

    Add support for handling ParameterMissing error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,11 @@ class ApplicationController < ActionController::Base
   def handle_errors
     begin
       yield
+    rescue ::ActionController::ParameterMissing => error
+      render status: 400, json: {
+               error: "Missing parameter: '#{error.param}'",
+               code: "PARAM_MISSING",
+             }
     rescue ErrorResponse => error
       render status: error.status, json: error.data
     end
