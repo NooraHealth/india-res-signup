5d48ec6b9a662dc4fc52841126a905880284c1f7
Sreeram <sreeramramasubramanian@NH-LT-0063.local>
2023-03-23 11:37:19 +0530

    Bug fix in migration

--- db/migrate/20230320201433_add_exophone_reference_to_user_signup_tracker.rb
+++ db/migrate/20230320201433_add_exophone_reference_to_user_signup_tracker.rb
@@ -1,6 +1,6 @@
 class AddExophoneReferenceToUserSignupTracker < ActiveRecord::Migration[5.2]
   def change
-    remove_column :user_signup_trackers, :exophone
+    # remove_column :user_signup_trackers, :exophone
     add_reference :user_signup_trackers, :exophone
   end
 end
