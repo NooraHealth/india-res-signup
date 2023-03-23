93ea37f8c3ce60dea6a3601a2470e146d9a50304
Sreeram <sreeramramasubramanian@NH-LT-0063.local>
2023-03-23 16:47:16 +0530

    Bug fix

--- db/migrate/20230323111219_add_more_fields_to_user_signup_tracker.rb
+++ db/migrate/20230323111219_add_more_fields_to_user_signup_tracker.rb
@@ -1,8 +1,7 @@
 class AddMoreFieldsToUserSignupTracker < ActiveRecord::Migration[5.2]
   def change
-    add_reference :user_signup_trackers, :state
-    add_column :user_signup_trackers, :call_sid, :string
-    add_column :user_signup_trackers, :sms_id, :string
-    add_reference :user_signup_trackers, :qr_code
+    #add_reference :user_signup_trackers, :state
+    #add_column :user_signup_trackers, :call_sid, :string
+    #add_column :user_signup_trackers, :sms_id, :string
   end
 end
