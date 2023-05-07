3708be67448a118e6669f8e268e9a0ee9f9c0b0c
Sreeram <sreeramramasubramanian@NH-LT-0063.local>
2023-05-07 23:25:00 +0530

    Added qr code to condition area and also added state_id as a necessary criteria to consider a user fully onboarded

---
+++ db/migrate/20230421133415_add_qr_code_id_to_textit_group.rb
@@ -0,0 +1,4 @@
+class AddQrCodeIdToTextitGroup < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_16_090051) do
+ActiveRecord::Schema.define(version: 2023_04_21_133415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -192,6 +192,7 @@ ActiveRecord::Schema.define(version: 2023_04_16_090051) do
     t.datetime "completed_at"
     t.string "call_status"
     t.string "campaign_sid"
+    t.string "call_direction"
     t.index ["condition_area_id"], name: "index_user_signup_trackers_on_condition_area_id"
     t.index ["exophone_id"], name: "index_user_signup_trackers_on_exophone_id"
     t.index ["language_id"], name: "index_user_signup_trackers_on_language_id"
